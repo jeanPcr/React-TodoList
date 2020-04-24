63bf01ec41d12057d202466362f194cea44d4117
jeanPcr <jean.coding971@gmail.com>
2020-04-23 22:56:17 -0400

    Updates

---
+++ precache-manifest.ffeebeaf1ff5f8c3aeeaca750e7b8cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517a4abf46acacd3fc12441b3b3ac572",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "01e4bd03c9ab0c907206",
+    "url": "/react-todolist/static/css/main.310d1734.chunk.css"
+  },
+  {
+    "revision": "86ffee95af522b8472de",
+    "url": "/react-todolist/static/js/2.27b3e655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.27b3e655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e4bd03c9ab0c907206",
+    "url": "/react-todolist/static/js/main.1ca5bfd3.chunk.js"
+  },
+  {
+    "revision": "6659e5fce766468a9b49",
+    "url": "/react-todolist/static/js/runtime-main.1dd42077.js"
+  }
+]);
